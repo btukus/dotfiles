a6c3b3a8d52b171b11a7bb8d34af6f42d427f986
btukus <berkhantukus@outlook.com>
2022-06-29 11:10:53 +0200

    Fix: Telescope ff doesnt add 'A' anymore'

--- nvim/.config/nvim/lua/plugins/install_plugins.lua
+++ nvim/.config/nvim/lua/plugins/install_plugins.lua
@@ -47,10 +47,10 @@ end
 
 return packer.startup(function(use)
 	-- Neovim Config
-	use({ "wbthomason/packer.nvim" }) -- Have packer manage itself
-	use({ "nvim-lua/plenary.nvim" }) -- Useful lua functions used by lots of plugins
-	use({ "lewis6991/impatient.nvim", config = get_setup("impatient") }) -- Improves startup time by implementing a cache
-	-- use { "famiu/nvim-reload", config = get_setup("nvim-reload") }                 -- Reloads/restarts nvim configuration
+	use({ "wbthomason/packer.nvim" })
+	use({ "nvim-lua/plenary.nvim" })
+	use({ "lewis6991/impatient.nvim", config = get_setup("impatient") })
+	-- use { "famiu/nvim-reload", config = get_setup("nvim-reload") }
 
 	-- Window and session management
 	use({ "ThePrimeagen/harpoon" })
@@ -59,21 +59,20 @@ return packer.startup(function(use)
 		opt = true,
 		cmd = { "ToggleTerm" },
 		config = get_setup("toggleterm"),
-	}) -- A neovim plugin to persist and toggle multiple terminals during an editing session
+	})
 	use({ "anuvyklack/hydra.nvim", config = get_setup("hydra") })
 
 	-- Text Editing
 	use({ "numToStr/Comment.nvim", event = "CursorMoved", config = get_setup("comment") })
-	use({ "JoosepAlviste/nvim-ts-context-commentstring", after = "Comment.nvim" }) -- A Neovim plugin for setting the commentstring option based on the cursor location in the file. The location is checked via treesitter queries.
-	use({ "windwp/nvim-autopairs", after = "nvim-cmp", config = get_setup("autopairs") }) -- Autopairs, integrates with both cmp and treesitter
+	use({ "JoosepAlviste/nvim-ts-context-commentstring", after = "Comment.nvim" })
+	use({ "windwp/nvim-autopairs", after = "nvim-cmp", config = get_setup("autopairs") })
 	use({ "windwp/nvim-ts-autotag" })
 
 	-- Buffer Plugins
-	use({ "moll/vim-bbye", opt = true, cmd = { "Bdelete" } }) -- Bbye allows you to do delete buffers (close files) without closing your windows or messing up your layout.
-	use({ "matbme/JABS.nvim", opt = true, cmd = { "JABSOpen" }, config = get_setup("jabs") })
+	use({ "moll/vim-bbye", opt = true, cmd = { "Bdelete" } })
 
 	-- UI
-	use({ "kyazdani42/nvim-web-devicons" }) -- Icons
+	use({ "kyazdani42/nvim-web-devicons" })
 	use({ "lukas-reineke/indent-blankline.nvim", event = "BufRead", config = get_setup("indentline") }) -- Adds indentation lines on blank lines
 	use({ "goolord/alpha-nvim", config = get_setup("alpha") })
 	use({
@@ -81,42 +80,42 @@ return packer.startup(function(use)
 		opt = true,
 		cmd = { "NvimTreeToggle", "NvimTreeFocus" },
 		config = get_setup("nvim-tree"),
-	}) -- File explorer tree written in lua
+	})
 	use({ "nvim-lualine/lualine.nvim", after = currentTheme, event = "BufWinEnter", config = get_setup("lualine") }) -- Statusline plugin
-	use({
-		"KadoBOT/nvim-spotify",
-		opt = true,
-		cmd = { "Spotify" },
-		config = get_setup("nvim-spotify"),
-		run = "make",
-	}) -- Spotify-tui integration
+	-- use({
+	-- 	"KadoBOT/nvim-spotify",
+	-- 	opt = true,
+	-- 	cmd = { "Spotify" },
+	-- 	config = get_setup("nvim-spotify"),
+	-- 	run = "make",
+	-- })
 
 	-- Colorschemes
-	use({ "lunarvim/darkplus.nvim" }) -- VScode theme
-	-- use({ "ful1e5/onedark.nvim" }) -- Current default
+	use({ "lunarvim/darkplus.nvim" })
+	-- use({ "ful1e5/onedark.nvim" })
 	-- use { "arcticicestudio/nord-vim" }
 	-- use { "jacoborus/tender.vim" }
 
 	-- Git
-	use({ "akinsho/git-conflict.nvim", config = get_setup("git-conflict") }) -- A plugin to visualise and resolve conflicts in neovim
+	use({ "akinsho/git-conflict.nvim", config = get_setup("git-conflict") })
 	use({ "ThePrimeagen/git-worktree.nvim", after = "telescope.nvim", config = get_setup("git-worktree") })
 	use({ "sindrets/diffview.nvim", opt = true, cmd = { "DiffviewOpen" } })
 
 	-- Cmp Plugins
-	use({ "hrsh7th/nvim-cmp", event = "InsertEnter", config = get_setup("cmp") }) -- The completion plugin
-	use({ "hrsh7th/cmp-buffer", after = "nvim-cmp" }) -- buffer completions
-	use({ "hrsh7th/cmp-path", after = "nvim-cmp" }) -- path completions
-	use({ "saadparwaiz1/cmp_luasnip", after = "nvim-cmp" }) -- snippet completions
+	use({ "hrsh7th/nvim-cmp", event = "InsertEnter", config = get_setup("cmp") })
+	use({ "hrsh7th/cmp-buffer", after = "nvim-cmp" })
+	use({ "hrsh7th/cmp-path", after = "nvim-cmp" })
+	use({ "saadparwaiz1/cmp_luasnip", after = "nvim-cmp" })
 	use({ "hrsh7th/cmp-nvim-lsp", after = "nvim-cmp" })
 	use({ "hrsh7th/cmp-nvim-lua", after = "nvim-cmp" })
 	use({ "tzachar/cmp-tabnine", after = "nvim-cmp", run = "./install.sh", config = get_setup("tabnine") })
-	use({ "L3MON4D3/LuaSnip", event = "InsertEnter" }) -- snippet engine
-	use({ "rafamadriz/friendly-snippets", event = "InsertEnter" }) -- a bunch of snippets to use
+	use({ "L3MON4D3/LuaSnip", event = "InsertEnter" })
+	use({ "rafamadriz/friendly-snippets", event = "InsertEnter" })
 
 	-- LSP
-	use({ "neovim/nvim-lspconfig", config = get_setup("lsp") }) -- enable LSP
-	use({ "williamboman/nvim-lsp-installer" }) -- simple to use language server installer
-	use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
+	use({ "neovim/nvim-lspconfig", config = get_setup("lsp") })
+	use({ "williamboman/nvim-lsp-installer" })
+	use({ "jose-elias-alvarez/null-ls.nvim" })
 	use({ "RRethy/vim-illuminate", config = get_setup("illuminate") })
 	use({ "mfussenegger/nvim-jdtls", event = "BufWinEnter" })
 
@@ -126,13 +125,13 @@ return packer.startup(function(use)
 		opt = true,
 		cmd = { "Telescope" },
 		config = get_setup("telescope"),
-	}) -- Fuzzy finder over lists. Items are shown in a popup with a prompt to search over.
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- It is a c port of the fzf algorithm
+	})
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 	use({ "nvim-telescope/telescope-file-browser.nvim" })
 	use({ "nvim-telescope/telescope-project.nvim" })
 
 	-- Treesitter
-	use({ "nvim-treesitter/nvim-treesitter", config = get_setup("treesitter") }) -- A parsing library that allows syntax highlighting, code nav, etc.
+	use({ "nvim-treesitter/nvim-treesitter", config = get_setup("treesitter") })
 	use({ "lewis6991/spellsitter.nvim", config = get_setup("spellsitter") })
 
 	-- Debugger
@@ -141,7 +140,7 @@ return packer.startup(function(use)
 		opt = true,
 		cmd = { "DapToggleBreakpoint" },
 		config = get_setup("dap"),
-	}) -- A Debug Adapter Protocol client implementation for Neovim
+	})
 	use({ "rcarriga/nvim-dap-ui", after = "nvim-dap" })
 	use({ "ravenxrz/DAPInstall.nvim", after = "nvim-dap" })
 
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,34 +1,59 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-  return
+	return
 end
 
-telescope.load_extension('fzf')
-telescope.load_extension('project')
-telescope.load_extension('file_browser')
+telescope.load_extension("fzf")
+telescope.load_extension("project")
+telescope.load_extension("file_browser")
 telescope.load_extension("git_worktree")
-telescope.load_extension('harpoon')
+telescope.load_extension("harpoon")
 
+local actions = require("telescope.actions")
 
-local actions = require "telescope.actions"
+telescope.setup({
+	defaults = {
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+		},
+		prompt_prefix = " ",
+		selection_caret = " ",
+		initial_mode = "insert",
+		sorting_strategy = "ascending",
+		layout_strategy = "horizontal",
+		layout_config = {
+			horizontal = {
+				prompt_position = "top",
+				preview_width = 0.55,
+				results_width = 0.8,
+			},
+			vertical = {
+				mirror = false,
+			},
+			width = 0.87,
+			height = 0.80,
+			preview_cutoff = 120,
+		},
 
-telescope.setup {
-  defaults = {
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "smart" },
-  },
-  pickers = {
-    telescope.extensions.project.project{}
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-    },
-    project = {
+		file_ignore_patterns = { "node_modules", ".git" },
+		path_display = { "smart" },
+	},
+	pickers = {},
+	extensions = {
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+			override_file_sorter = true,
+			case_mode = "smart_case",
+		},
+		project = {
+      initial_mode = "normal",
       base_dirs = {
         '~/dev/src',
         {'~/dev/src2'},
@@ -36,13 +61,13 @@ telescope.setup {
         {path = '~/dev/src4'},
         {path = '~/dev/src5', max_depth = 2},
       },
-      hidden_files = true, -- default: false
+      hidden_files = true,
       theme = "dropdown"
-    },
-    file_browser = {
-      theme = "ivy",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = true,
-    },
-  }
-}
+		},
+		file_browser = {
+      initial_mode = "normal",
+			theme = "ivy",
+			hijack_netrw = true,
+		},
+	},
+})

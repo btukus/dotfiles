d3b77829cd4a5bb19c46a16cafd1f80b72783f7f
btukus <berkhantukus@outlook.com>
2022-07-03 18:50:38 +0200

    Fix: Clipboard is called by autocmd

--- nvim/.config/nvim/lua/plugins/install_plugins.lua
+++ nvim/.config/nvim/lua/plugins/install_plugins.lua
@@ -114,7 +114,7 @@ return packer.startup(function(use)
 	use({ "sindrets/diffview.nvim", opt = true, cmd = { "DiffviewOpen" } })
 
 	-- Cmp Plugins
-	use({ "hrsh7th/nvim-cmp", event = "InsertEnter", config = get_setup("cmp") })
+	use({ "hrsh7th/nvim-cmp", event = "InsertEnter *", config = get_setup("cmp") })
 	use({ "hrsh7th/cmp-buffer", after = "nvim-cmp" })
 	use({ "hrsh7th/cmp-path", after = "nvim-cmp" })
 	use({ "saadparwaiz1/cmp_luasnip", after = "nvim-cmp" })

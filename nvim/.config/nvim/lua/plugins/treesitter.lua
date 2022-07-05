8f27681438d5ad07e1a2bb6570742962bc192fba
btukus <berkhantukus@outlook.com>
2022-07-05 10:34:01 +0200

    Refactored installed languages treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,47 +3,46 @@ if not status_ok then
 	return
 end
 
+local languages = { -- Object Oriented
+	"c",
+	"c_sharp", -- Nvim gives error that not available
+	"cpp",
+	"java",
+	"kotlin", -- Unable to find zip file
+	"scala",
+	"python",
+	"dart",
+	"go",
+
+	-- Front-end
+	"css",
+	"scss",
+	"html",
+	"javascript",
+	"typescript",
+
+	-- Scripting
+	"lua",
+	"vim",
+	"bash",
+
+	-- Declarative
+	"yaml",
+	"json",
+	"hcl",
+	"dockerfile",
+
+	-- Request languages
+	"graphql",
+
+	-- Misc
+	"http",
+	"regex",
+	"markdown",
+	"latex",
+}
 configs.setup({
-	ensure_installed = {
-    -- Object Oriented
-    "c",
-    "c_sharp",  -- Nvim gives error that not available
-    "cpp",
-    "java",
-    "kotlin",     -- Unable to find zip file 
-    "scala",
-    "python",
-    "dart",
-    "go",
-
-    -- Front-end
-    "css",
-    "scss",
-    "html",
-    "javascript",
-    "typescript",
-
-    -- Scripting
-    "lua",
-    "vim",
-    "bash",
-
-    -- Declarative
-    "yaml",
-    "json",
-    "hcl",
-    "dockerfile",
-
-    -- Request languages
-    "graphql",
-
-    -- Misc
-    "http",
-    "regex",
-    "markdown",
-    "latex"
-    
-  }, -- one of "all" or a list of languages
+	ensure_installed = languages,
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
@@ -51,8 +50,8 @@ configs.setup({
 	autopairs = {
 		enable = true,
 	},
-  autotag = {
-    enable = true,
-  },
+	autotag = {
+		enable = true,
+	},
 	indent = { enable = true, disable = { "python", "css" } },
 })

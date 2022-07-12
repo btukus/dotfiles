f9e33e743510abd7584fdb8b978803c4e16e7173
btukus <berkhan.tukus@capgemini.com>
2022-07-12 13:40:21 +0200

    Feat: Spotify; Feat: Tmux

--- nvim/.config/nvim/lua/plugins/nvim-spotify.lua
+++ nvim/.config/nvim/lua/plugins/nvim-spotify.lua
@@ -1,9 +1,12 @@
-local spotify = require'nvim-spotify'
+local status_ok, spotify = pcall(require, "nvim-spotify")
+if not status_ok then
+	return
+end
 
-spotify.setup {
-    -- default opts
-    status = {
-        update_interval = 10000, -- the interval (ms) to check for what's currently playing
-        format = '%s %t by %a' -- spotify-tui --format argument
-    }
-}
+spotify.setup({
+	-- default opts
+	status = {
+		update_interval = 10000, -- the interval (ms) to check for what's currently playing
+		format = "%s %t by %a", -- spotify-tui --format argument
+	},
+})

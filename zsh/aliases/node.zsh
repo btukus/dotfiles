ec4d37510ccf3733591d7b5966150fcd9c67c7a4
btukus <berkhan.tukus@capgemini.com>
2022-12-20 21:33:56 +0100

    Adds yarn aliases

--- zsh/aliases/node.zsh
+++ zsh/aliases/node.zsh
@@ -5,4 +5,14 @@ alias nr='npm run'
 alias ns='npm start'
 alias nt='npm test'
 alias nb='npm run-script build'
-alias nrs='run-script'
+alias nrs='npm run-script'
+
+
+# Yarn aliases
+alias y='yarn'
+alias yi='yarn install'
+# alias nil='npm install --legacy-peer-deps'
+alias yr='yarn run'
+alias ys='yarn run start'
+alias yt='yarn run test'
+alias yb='npm run build'

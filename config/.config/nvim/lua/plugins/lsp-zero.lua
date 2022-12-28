d1bc5314819bc2c4328e96b54b3e5d687258d8d7
btukus <berkhan.tukus@capgemini.com>
2022-12-28 12:54:08 +0100

    fixes go definition of tsserver

--- config/.config/nvim/lua/plugins/lsp-zero.lua
+++ config/.config/nvim/lua/plugins/lsp-zero.lua
@@ -72,6 +72,37 @@ lsp.on_attach(function(client, bufnr)
   lsp_keymaps(bufnr)
 end)
 
+
+lsp.configure('tsserver', {
+  handlers = {
+      ["textDocument/definition"] = function(_, result, params)
+        if result == nil or vim.tbl_isempty(result) then
+          --[[ local _ = vim.lsp.log.info() and vim.lsp.log.info(params.method, 'No location found') ]]
+          return nil
+        end
+
+        if vim.tbl_islist(result) then
+          vim.lsp.util.jump_to_location(result[1])
+          if #result > 1 then
+            local isReactDTs = false
+            for key, value in pairs(result) do
+              if string.match(value.targetUri, '%.d.ts') then
+                isReactDTs = true
+                break
+              end
+            end
+            if not isReactDTs then
+              vim.fn.setqflist(vim.lsp.util.locations_to_items(result))
+              vim.api.nvim_command("copen")
+            end
+          end
+        else
+          vim.lsp.util.jump_to_location(result)
+        end
+    end
+  };
+})
+
 lsp.nvim_workspace()
 
 lsp.setup()

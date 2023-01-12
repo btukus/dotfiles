ee0d31999a836112db92aa982a1d6fdfc2edd161
btukus <berkhan.tukus@capgemini.com>
2023-01-12 10:40:46 +0100

    Updated location when path is added

--- install_brew.sh
+++ install_brew.sh
@@ -1,14 +1,13 @@
 systemtype=$(uname -s)
 
+# Install Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 if [[ "$systemtype" == "Linux" ]]; then
   echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$(whoami)/.profile
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   sudo apt-get install build-essential python3-pip -y
 fi
-
-# Install Brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 
 # Add brew to the shell
 if [[ "$systemtype" == "Darwin" ]]; then

2f26db0c143bf559788986066da271a1039f0aea
btukus <berkhan.tukus@capgemini.com>
2023-01-22 22:50:20 +0100

    Raycast removed

--- config/.config/nvim/lua/plugins/indentline.lua
+++ config/.config/nvim/lua/plugins/indentline.lua
@@ -7,7 +7,7 @@ indent_blankline.setup {
   show_current_context = true,
   indent_blankline_char = "▏",
   indent_blankline_show_trailing_blankline_indent = false,
-  indent_blankline_show_first_indent_level = true,
+  show_first_indent_level = false,
   indent_blankline_use_treesitter = true,
   indent_blankline_show_current_context = true,
   indent_blankline_buftype_exclude = { "terminal", "nofile" },

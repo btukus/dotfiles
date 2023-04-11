fc6a70ec69cb6b5687975d780822a653c4e64dc5
btukus <berkhantukus@outlook.com>
2023-04-11 13:31:35 +0200

    docker aliases

--- zsh/aliases/docker.zsh
+++ zsh/aliases/docker.zsh
@@ -1,8 +1,15 @@
 # Docker aliases
 alias d='docker'
+alias dps='docker ps'
+alias di='docker images'
+
+de () {
+  docker exec -it $1 /bin/bash;
+}
 
 # Docker compose
 alias dc='docker-compose'
 alias dcu='docker-compose up -d'
-alias dcub='docker-compose up --build'
+alias dcub='docker-compose up --build -d'
+alias dcubl='docker-compose up --build'
 alias dspa='docker system prune --all'

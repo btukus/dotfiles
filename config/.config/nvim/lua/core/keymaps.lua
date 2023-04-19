f47ee229e09ac6da7e185e258a5ea4bf33fa0482
btukus <berkhantukus@outlook.com>
2023-04-19 10:47:49 +0200

    Remove keymap shortcut

--- config/.config/nvim/lua/core/keymaps.lua
+++ config/.config/nvim/lua/core/keymaps.lua
@@ -1,67 +1,67 @@
 -- Shorten function name
-local keymap = vim.keymap.set
--- Silent keymap option
+--[[ local vim.keymap.set = vim.keymap.set ]]
+-- Silent vim.keymap.set option
 local opts = { silent = true }
 
 --Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
+vim.keymap.set("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Normal --
 -- Close Neovim
-keymap("n", "<leader>qq", ":q!<CR>", opts)
-keymap("n", "<leader>wq", ":wq<CR>", opts)
+vim.keymap.set("n", "<leader>qq", ":q!<CR>", opts)
+vim.keymap.set("n", "<leader>wq", ":wq<CR>", opts)
 
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
 
 -- Split window
-keymap("n", "<leader>sj", "<C-w>s", opts)
-keymap("n", "<leader>sk", "<C-w>v", opts)
-keymap("n", "<leader>sh", "<C-w>q", opts)
-keymap("n", "<leader>sl", "<C-w>o", opts)
+vim.keymap.set("n", "<leader>sj", "<C-w>s", opts)
+vim.keymap.set("n", "<leader>sk", "<C-w>v", opts)
+vim.keymap.set("n", "<leader>sh", "<C-w>q", opts)
+vim.keymap.set("n", "<leader>sl", "<C-w>o", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Clear highlights
-keymap("n", "<leader>c", "<cmd>nohlsearch<CR>", opts)
+vim.keymap.set("n", "<leader>c", "<cmd>nohlsearch<CR>", opts)
 
 -- Close buffers
-keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
+vim.keymap.set("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
 
 -- Insert --
-keymap("n", "<leader>yy", ":%y+<cr>", opts)
+vim.keymap.set("n", "<leader>yy", ":%y+<cr>", opts)
 
 -- Make visual mode consistent with other settings
-keymap("v", "v", "<esc>V", opts)
-keymap("n", "V", "v$h", opts)
-keymap("n", "Y", "v$hy", opts)
-keymap("n", "<leader>vg", "ggVG", opts)
+vim.keymap.set("v", "v", "<esc>V", opts)
+vim.keymap.set("n", "V", "v$h", opts)
+vim.keymap.set("n", "Y", "v$hy", opts)
+vim.keymap.set("n", "<leader>vg", "ggVG", opts)
 
 -- Use backspace key for matching parens
-keymap("n", "<BS>", "%", opts)
-keymap("x", "<BS>", "%", opts)
+vim.keymap.set("n", "<BS>", "%", opts)
+vim.keymap.set("x", "<BS>", "%", opts)
 
 -- Don't open command history
-keymap("n", "q:", "<nop>", opts)
+vim.keymap.set("n", "q:", "<nop>", opts)
 
 -- Don't copy when pasted
---[[ keymap("v", "p", '"_dP`]', opts) ]]
+--[[ vim.keymap.set("v", "p", '"_dP`]', opts) ]]
 
 -- Don't yank when x
-keymap("n", "x", '"_x', opts)
+vim.keymap.set("n", "x", '"_x', opts)
 
 -- Auto indentation on empty lines
 function autoIndent(key)
@@ -70,71 +70,71 @@ function autoIndent(key)
 	end
 end
 
-vim.keymap.set("n", "i", autoIndent("i"), { expr = true, noremap = true })
-vim.keymap.set("n", "I", autoIndent("I"), { expr = true, noremap = true })
-vim.keymap.set("n", "a", autoIndent("a"), { expr = true, noremap = true })
-vim.keymap.set("n", "A", autoIndent("A"), { expr = true, noremap = true })
+vim.vim.keymap.set.set("n", "i", autoIndent("i"), { expr = true, noremap = true })
+vim.vim.keymap.set.set("n", "I", autoIndent("I"), { expr = true, noremap = true })
+vim.vim.keymap.set.set("n", "a", autoIndent("a"), { expr = true, noremap = true })
+vim.vim.keymap.set.set("n", "A", autoIndent("A"), { expr = true, noremap = true })
 
 ------------------------------- Plugins ------------------------------------
 
 -- Packer
-keymap("n", "<leader>ps", ":PackerSync<CR>", opts)
-keymap("n", "<leader>pp", ":PackerProfile<CR>", opts)
-keymap("n", "<leader>pS", ":PackerStatus<CR>", opts)
+vim.keymap.set("n", "<leader>ps", ":PackerSync<CR>", opts)
+vim.keymap.set("n", "<leader>pp", ":PackerProfile<CR>", opts)
+vim.keymap.set("n", "<leader>pS", ":PackerStatus<CR>", opts)
 
 -- Telescope
-keymap("n", "<leader>ff", ":Telescope find_files hidden=true<CR>", opts)
-keymap("n", "<leader>fd", ":Telescope live_grep<CR>", opts)
-keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
-keymap("n", "<leader>fr", ":Telescope file_browser hidden=true<CR> ", opts)
-keymap("n", "<leader>ft", ":Telescope file_browser hidden=true path=%:p:h select_buffer=true<CR> ", opts)
-keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
-keymap("n", "<leader>fa", ":Telescope terraform_doc full_name=hashicorp/azurerm<CR>", opts)
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files hidden=true<CR>", opts)
+vim.keymap.set("n", "<leader>fd", ":Telescope live_grep<CR>", opts)
+vim.keymap.set("n", "<leader>fp", ":Telescope projects<CR>", opts)
+vim.keymap.set("n", "<leader>fr", ":Telescope file_browser hidden=true<CR> ", opts)
+vim.keymap.set("n", "<leader>ft", ":Telescope file_browser hidden=true path=%:p:h select_buffer=true<CR> ", opts)
+vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+vim.keymap.set("n", "<leader>fa", ":Telescope terraform_doc full_name=hashicorp/azurerm<CR>", opts)
 
 
 -- Nvim-Tree
-keymap("n", "<leader>nn", ":NvimTreeToggle<CR>", opts)
+vim.keymap.set("n", "<leader>nn", ":NvimTreeToggle<CR>", opts)
 
 -- Git Conflict
-keymap("n", "<leader>gl", "GitConflictListQf<CR>", opts)
+vim.keymap.set("n", "<leader>gl", "GitConflictListQf<CR>", opts)
 
 -- Git Diffview
-keymap("n", "<leader>gv", ":DiffviewOpen<CR>", opts)
-keymap("n", "<leader>gc", ":DiffviewClose<CR>", opts)
+vim.keymap.set("n", "<leader>gv", ":DiffviewOpen<CR>", opts)
+vim.keymap.set("n", "<leader>gc", ":DiffviewClose<CR>", opts)
 
 -- Git-worktree
-keymap("n", "<leader>gt", "<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", opts)
+vim.keymap.set("n", "<leader>gt", "<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", opts)
 
 -- Lazygit
-keymap("n", "<leader>gg", ":LazyGit<CR>", opts)
+vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", opts)
 
 -- Harpoon
-keymap("n", "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<CR>", opts)
-keymap("n", "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", opts)
-keymap("n", "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<CR>", opts)
-keymap("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<CR>", opts)
+vim.keymap.set("n", "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<CR>", opts)
+vim.keymap.set("n", "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", opts)
+vim.keymap.set("n", "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<CR>", opts)
+vim.keymap.set("n", "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<CR>", opts)
 
 -- TroubleToggle
-keymap("n", "<leader>dd", ":TroubleToggle<CR>", opts)
+vim.keymap.set("n", "<leader>dd", ":TroubleToggle<CR>", opts)
 
 -- SymbolOutline
-keymap("n", "<leader>so", ":SymbolsOutline<CR>", opts)
+vim.keymap.set("n", "<leader>so", ":SymbolsOutline<CR>", opts)
 
 -- Git-conflict
-keymap("n", "co", "<Plug>(git-conflict-ours)")
-keymap("n", "<leader>ll", ":GitConflictListQf<CR>")
-keymap("n", "ct", "<Plug>(git-conflict-theirs)")
-keymap("n", "cb", "<Plug>(git-conflict-both)")
-keymap("n", "c0", "<Plug>(git-conflict-none)")
-keymap("n", "]x", "<Plug>(git-conflict-prev-conflict)")
-keymap("n", "[x", "<Plug>(git-conflict-next-conflict)")
+vim.keymap.set("n", "co", "<Plug>(git-conflict-ours)")
+vim.keymap.set("n", "<leader>ll", ":GitConflictListQf<CR>")
+vim.keymap.set("n", "ct", "<Plug>(git-conflict-theirs)")
+vim.keymap.set("n", "cb", "<Plug>(git-conflict-both)")
+vim.keymap.set("n", "c0", "<Plug>(git-conflict-none)")
+vim.keymap.set("n", "]x", "<Plug>(git-conflict-prev-conflict)")
+vim.keymap.set("n", "[x", "<Plug>(git-conflict-next-conflict)")
 
 -- ThePrimeagen
-keymap("v", "J", ":m '>+1<CR>gv=gv")
-keymap("v", "K", ":m '<-2<CR>gv=gv")
-keymap("x", "L", ">gv", opts)
-keymap("x", "H", "<gv", opts)
-keymap("x", "<leader>p", "\"_dP")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("x", "L", ">gv", opts)
+vim.keymap.set("x", "H", "<gv", opts)
+vim.keymap.set("x", "<leader>p", "\"_dP")
 
 -- TreeSJ
-keymap("n", "<leader>tt", ":TSJToggle<CR>", opts)
+vim.keymap.set("n", "<leader>tt", ":TSJToggle<CR>", opts)

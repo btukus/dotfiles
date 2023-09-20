8538e33ac82a6bbb36a19c6d8c5b1efcc2617a01
btukus <berkhantukus@outlook.com>
2023-09-20 13:31:32 +0300

    Function for replacing username in https git link

--- zsh/aliases/functions.zsh
+++ zsh/aliases/functions.zsh
@@ -58,10 +58,25 @@ function git_develop_branch() {
 }
 
 pat() {
-  # Extract the original URL and new username and text
+  # Extract the original URL and the value for the switch
   local original_url="$1"
-  local new_text="$2"
+  local switch_value="$2"
   local new_username="${3:-btukus}"  # Default to "btukus" if no input is provided
+
+  # Determine new_text based on the switch_value
+  local new_text=""
+  case "$switch_value" in
+    1)
+      new_text="${ENECO_1:-default_value}"  # Use the value of KEY or "default_value" if KEY is not set
+      ;;
+    2)
+      new_text="${ENECO_2:-default_value}"
+      ;;
+    *)
+      echo "Invalid switch value. Please provide 1 or 2."
+      return 1
+      ;;
+  esac
 
   # Replace the username in the URL
   local modified_url=$(echo "$original_url" | awk -v user="$new_username" -v text="$new_text" -F'//' 'BEGIN{OFS="//"} {split($2,a,"@"); split(a[1],b,":"); sub(b[1],user":"text,a[1]); print $1, a[1]"@"a[2]}')

1e3be77039e4b329e3feb434c7c1cd6e89d07bb0
btukus <berkhantukus@outlook.com>
2023-12-02 14:31:02 +0100

    table.unpack 5.1 compatibility

--- config/.config/nvim/lua/plugins/lsp-zero.lua
+++ config/.config/nvim/lua/plugins/lsp-zero.lua
@@ -99,6 +99,7 @@ local luasnip = require("luasnip")
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_lua").lazy_load({ paths = { "~/.config/nvim/snippets" } })
 
+table.unpack = table.unpack or unpack -- 5.1 compatibility
 local has_words_before = function()
   local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil

6202fbd2c779a6d92d282cb5907796759d745967
btukus <berkhantukus@outlook.com>
2023-12-02 14:23:24 +0100

    Add tab in cmp

--- config/.config/nvim/lua/plugins/lsp-zero.lua
+++ config/.config/nvim/lua/plugins/lsp-zero.lua
@@ -4,9 +4,8 @@ if not cmp_status_ok then
 end
 lsp.preset('recommended')
 
-require("luasnip.loaders.from_vscode").lazy_load()
-require("luasnip.loaders.from_lua").lazy_load({ paths = { "~/.config/nvim/snippets" } })
 
+-- Config for lspkind
 require("mason").setup()
 require('mason-lspconfig').setup({
   ensure_installed = {
@@ -39,31 +38,7 @@ require('mason-lspconfig').setup({
   },
 })
 
--- local cfg = require("yaml-companion").setup({})
--- require("lspconfig")["yamlls"].setup(cfg)
-
-local cmp = require('cmp')
-cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp", group_index = 2 },
-    { name = "nvim_lua", group_index = 2 },
-    { name = "luasnip",  group_index = 2 },
-    { name = "buffer",   group_index = 2 },
-    { name = "path",     group_index = 2 },
-  }),
-  window = {
-    completion = cmp.config.window.bordered()
-  },
-  experimental = {
-    ghost_text = false,
-  },
-
-})
-
-
+-- Config for lspkind
 local function lsp_keymaps(bufnr)
   local opts = { noremap = true, silent = true }
   local keymap = vim.api.nvim_buf_set_keymap
@@ -117,5 +92,60 @@ lsp.configure('tsserver', {
     end
   },
 })
+
+
+-- Config for cmp
+local luasnip = require("luasnip")
+require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_lua").lazy_load({ paths = { "~/.config/nvim/snippets" } })
+
+local has_words_before = function()
+  local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local cmp = require('cmp')
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp", group_index = 2 },
+    { name = "nvim_lua", group_index = 2 },
+    { name = "luasnip",  group_index = 2 },
+    { name = "buffer",   group_index = 2 },
+    { name = "path",     group_index = 2 },
+  }),
+  window = {
+    completion = cmp.config.window.bordered()
+  },
+  experimental = {
+    ghost_text = false,
+  },
+
+})
+
 
 lsp.setup()

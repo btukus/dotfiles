d7f8797492ec6d0480172474d9dc5d0d66fb2824
btukus <berkhantukus@outlook.com>
2023-12-18 22:22:39 +0100

    Removed vscode extensions from brewfile

--- zsh/aliases/brew.zsh
+++ zsh/aliases/brew.zsh
@@ -3,5 +3,5 @@ alias br='brew reinstall'
 alias bu='brew uninstall'
 
 # Brewfile
-alias brew-export-mac='brew bundle dump --file=~/dotfiles/brew/Brewfile.macos --force'
-alias brew-export-linux='brew bundle dump --file=~/dotfiles/brew/Brewfile.linux --force'
+alias brew-export-mac='brew bundle dump --file=~/dotfiles/brew/Brewfile.macos --force && sed -i "" "/^vscode/d" ~/dotfiles/brew/Brewfile.macos'
+alias brew-export-linux='brew bundle dump --file=~/dotfiles/brew/Brewfile.linux --force && sed -i "" "/^vscode/d" ~/dotfiles/brew/Brewfile.linux'

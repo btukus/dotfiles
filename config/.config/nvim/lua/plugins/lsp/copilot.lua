26cec81bc649b345861848e62f365eb994ce3670
btukus <berkhantukus@outlook.com>
2024-08-29 13:08:00 +0200

    Copilot ghost text activated

--- config/.config/nvim/lua/plugins/lsp/copilot.lua
+++ config/.config/nvim/lua/plugins/lsp/copilot.lua
@@ -1,7 +1,21 @@
 return {
   "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
+  -- cmd = "Copilot",
   event = "CursorMoved",
-  config = true,
+  -- config = true,
   enabled = true,
+  config = function()
+    require("copilot").setup({
+      panel = {
+        enabled = false,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<M-v>",
+        },
+      },
+    })
+  end,
 }

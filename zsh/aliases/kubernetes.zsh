ad9c1a1ee7225f01ab13f2f2905386fcafd191ab
btukus <berkhantukus@outlook.com>
2024-09-12 14:50:42 +0200

    Conclusion added

--- zsh/aliases/kubernetes.zsh
+++ zsh/aliases/kubernetes.zsh
@@ -15,8 +15,33 @@ alias kfix='kustomize edit fix --vars'
 alias kbuild='kustomize build'
 
 # k9s
-alias ks='k9s'
+ks() {
+  if [ "$1" = "d" ]; then
+    kubectl config use-context sensey-dev-aks && k9s
+  elif [ "$1" = "p" ]; then
+    kubectl config use-context sensey-prod-aks && k9s
+  else
+    k9s;
+  fi
+}
 
+ksns () {
+  if [ "$1" = "t" ]; then
+    kubectl config use-context AKS20AKSINFOPLUS300-T && k9s
+  elif [ "$1" = "a" ]; then
+    kubectl config use-context AKS20AKSINFOPLUS200-A && k9s
+  elif [ "$1" = "p" ]; then
+    kubectl config use-context AKS20AKSINFOPLUS200-P && k9s
+  elif [ "$1" = "ht" ]; then
+    kubectl config use-context AKS20REISINFO100-T && k9s
+  elif [ "$1" = "ha" ]; then
+    kubectl config use-context AKS20REISINFO100-A && k9s
+  elif [ "$1" = "hp" ]; then
+    kubectl config use-context AKS20REISINFO100-P && k9s
+  else
+    k9s;
+  fi
+}
 
 # k8s resources
 alias busy='kubectl apply -f ${HOME}/drive/mac/development/k8s_resources/busybox.yaml'

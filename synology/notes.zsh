4682891c39889de7dcbeef2285b2b4456661a9d4
btukus <berkhantukus@outlook.com>
2025-02-05 21:44:32 +0100

    Added synology files

---
+++ synology/notes.zsh
@@ -0,0 +1,5 @@
+# Add node_modules to filter file
+sudo nvim ~/Library/Application\ Support/SynologyDrive/SynologyDrive.app/Contents/Resources/conf/filter
+
+# Add node_modules to filter-v4150 file
+sudo nvim ~/Library/Application\ Support/SynologyDrive/SynologyDrive.app/Contents/Resources/conf/filter-v4150

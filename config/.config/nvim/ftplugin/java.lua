2ed10d77c2a4d9a35101323331385599c9d8d18d
btukus <berkhantukus@outlook.com>
2025-08-01 16:17:20 +0200

    removed lsp settings

--- config/.config/nvim/ftplugin/java.lua
+++
@@ -1,5 +0,0 @@
-if not vim.g.vscode then
-	vim.opt_local.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
-	vim.opt_local.softtabstop = 4 -- Number of spaces that a <Tab> counts for while performing editing operations
-	vim.opt_local.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
-end

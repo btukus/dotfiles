02b1cf76f5a40d617a21c363daac29731f011667
btukus <berkhan.tukus@sensey.io>
2025-10-02 13:47:12 +0200

    Updated

--- zsh/aliases/git/reset-to-commit.zsh
+++ zsh/aliases/git/reset-to-commit.zsh
@@ -1,12 +1,12 @@
 # Reset to commit SHA
 
 function greset() {
-  if [-z "$1"]; then
+  if [ -z "$1" ]; then
     echo "Please provide a commit SHA"
     return 1
   fi
 
-  if [-z "$2"]; then
+  if [ -z "$2" ]; then
     echo "Please provide branch name"
     return 1
   fi
